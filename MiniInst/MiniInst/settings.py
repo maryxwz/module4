2bae561a701bfb69cc80c3c5322e7f602312ced4
nigniger <viktorchobotar2022@gmail.com>
2025-08-01 22:05:22 +0300

    Update settings.py

--- MiniInst/MiniInst/settings.py
+++ MiniInst/MiniInst/settings.py
@@ -120,3 +120,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'users.CustomUser'
+LOGIN_REDIRECT_URL = 'profile'
+LOGOUT_REDIRECT_URL = 'login'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
+TEMPLATES[0]['DIRS'] = [BASE_DIR / 'templates']
