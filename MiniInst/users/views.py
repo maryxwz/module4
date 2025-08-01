baae104f8573d87908687fd08ec10d520406a306
nigniger <viktorchobotar2022@gmail.com>
2025-08-01 22:02:02 +0300

    Update views.py

--- MiniInst/users/views.py
+++ MiniInst/users/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
+from .forms import RegisterForm
 
-# Create your views here.
+def register_view(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST, request.FILES)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('profile')
+    else:
+        form = RegisterForm()
+    return render(request, 'register.html', {'form': form})
+
+@login_required
+def profile_view(request):
+    return render(request, 'profile.html', {'user': request.user})
