ece286155d0a92cf0195d2dd775a651ea87a79b7
kiuter1 <ytigraman875@gmail.com>
2025-08-02 13:42:10 +0300

    create admin for CustomUser model

--- MiniInst/users/admin.py
+++ MiniInst/users/admin.py
@@ -1,3 +1,37 @@
 from django.contrib import admin
+from users.models import CustomUser
 
-# Register your models here.
+
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "username",
+        "email",
+        "bio",
+        "last_login",
+        "date_joined",
+        "is_private",
+        "is_staff",
+        "is_active",
+    )
+    list_display_links = (
+        "id",
+        "username",
+        "email",
+    )
+    list_filter = (
+        "id",
+        "username",
+        "email",
+        "last_login",
+        "date_joined",
+        "is_private",
+        "is_staff",
+        "is_active",
+    )
+    search_fields = (
+        'id',
+        "username",
+        "email",
+    )
