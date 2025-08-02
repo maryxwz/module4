2829edb227f1d354585951f2c49014928f0811c7
kiuter1 <ytigraman875@gmail.com>
2025-08-02 14:07:50 +0300

    create admin for Post model

--- MiniInst/posts/admin.py
+++ MiniInst/posts/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from posts.models import Post
 
-# Register your models here.
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "author",
+        "caption",
+        "is_archived",
+        "created_at",
+    )
+    list_display_links = (
+        "id",
+        "author",
+    )
+    list_filter = (
+        "id",
+        "author",
+        "is_archived",
+        "created_at",
+    )
+    search_fields = (
+        "id",
+        "author",
+        "caption",
+        "is_archived",
+    )
