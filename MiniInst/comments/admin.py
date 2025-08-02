c67fbdc27edfa90598dfdd5f5a216ac849324dd2
kiuter1 <ytigraman875@gmail.com>
2025-08-02 15:11:32 +0300

    create admin for Comment model

--- MiniInst/comments/admin.py
+++ MiniInst/comments/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from comments.models import Comment
 
-# Register your models here.
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "author",
+        "post",
+        "is_deleted",
+        "created_at",
+    )
+    list_display_links = (
+        "id",
+        "author",
+        "post",
+    )
+    list_filter = (
+        "id",
+        "author",
+        "post",
+        "is_deleted",
+        "created_at",
+    )
+    search_fields = (
+        "id",
+        "author",
+        "post",
+        "is_deleted",
+        "is_archived",
+    )
+
