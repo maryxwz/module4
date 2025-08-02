aa9412cdc53409b6787b76080f84a50d5c0a6a1b
kiuter1 <ytigraman875@gmail.com>
2025-08-02 15:24:08 +0300

    create admin for Story model

--- MiniInst/stories/admin.py
+++ MiniInst/stories/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from stories.models import Story
 
-# Register your models here.
+
+@admin.register(Story)
+class StoryAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "author",
+        "is_archived",
+        "expires_at",
+        "created_at",
+    )
+    list_display_links = (
+        "id",
+        "author",
+    )
+    list_filter = (
+        "id",
+        "author",
+        "is_archived",
+        "expires_at",
+        "created_at",
+    )
+    search_fields = (
+        "id",
+        "author",
+        "is_archived",
+        "expires_at",
+    )
+
