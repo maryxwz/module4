4536eb00b1cea00feaf88e310d1898b7acd89bb3
kiuter1 <ytigraman875@gmail.com>
2025-08-10 18:48:58 +0300

    create admin for UserReport model

--- MiniInst/backoffice/admin.py
+++ MiniInst/backoffice/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from backoffice.models import UserReport
 
-# Register your models here.
+
+@admin.register(UserReport)
+class UserReportAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "author",
+        "description",
+        "created_at",
+    )
+    list_display_links = (
+        "id",
+        "author",
+    )
+    list_filter = (
+        "id",
+        "author",
+        "created_at",
+    )
+    search_fields = (
+        "id",
+        "author",
+    )
+
