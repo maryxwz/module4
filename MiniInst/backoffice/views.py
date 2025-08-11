ef0514a01abdd7b7f3386cc1c8fef0d43863aa3b
kiuter1 <ytigraman875@gmail.com>
2025-08-11 18:37:35 +0300

    fix views import

--- MiniInst/backoffice/views.py
+++ MiniInst/backoffice/views.py
@@ -1,7 +1,9 @@
 from django.contrib.admin.views.decorators import staff_member_required
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
+
 from backoffice.models import UserReport
+
 
 @staff_member_required
 def user_reports_list(request: HttpRequest) -> HttpResponse:
