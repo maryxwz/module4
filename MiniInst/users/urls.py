edf4b9f78c5180c8fc778a7ea6e14a7d3fe1588e
nigniger <viktorchobotar2022@gmail.com>
2025-08-21 21:37:46 +0300

    Update urls.py

--- MiniInst/users/urls.py
+++ MiniInst/users/urls.py
@@ -5,5 +5,6 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('register/', register_view, name='register'),
     path('profile/', profile_view, name='profile'),
-    path('accounts/login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
