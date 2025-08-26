d1a9e44bd6b94e31f057e2225a450e65fec46e3c
Vova <vovinator2010@gmail.com>
2025-08-26 21:07:44 +0300

    Now if user got a closed profile, no one can reach it through an urlpattern. After login or registration user is getting redirected to his profile page.

--- MiniInst/users/views.py
+++ MiniInst/users/views.py
@@ -1,9 +1,16 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
 from .forms import CustomUserCreationForm
-
+from django.urls import reverse_lazy
+from django.contrib.auth import views as auth_views
 from .models.custom_user import CustomUser
+from django.core.exceptions import PermissionDenied
+
+
+class CustomLoginView(auth_views.LoginView):
+    def get_success_url(self):
+        return reverse_lazy("profile", args=[self.request.user.username]) # щоб після логіну кидало на профіль цього юзера
 
 def register_view(request):
     if request.method == 'POST':
@@ -11,12 +18,16 @@ def register_view(request):
         if form.is_valid():
             user = form.save()
             login(request, user)
-            return redirect('profile')
+            return redirect('profile',username=request.user.username)
     else:
         form = CustomUserCreationForm()
     return render(request, 'register.html', {'form': form})
 
 
 @login_required
-def profile_view(request):
-    return render(request, 'profile.html', {'user': request.user})
+def profile_view(request, username):
+    profile_user = get_object_or_404(CustomUser, username=username)
+    print('Looking for', profile_user) # перевірка чи підтягнувся юзернейм
+    if request.user.username != profile_user.username or profile_user.is_private == True:
+        raise PermissionDenied("Цей профіль закритий") # ось тут замінити на редірект до сторінки (таск в трело)
+    return render(request, 'profile.html', {'user': profile_user})
