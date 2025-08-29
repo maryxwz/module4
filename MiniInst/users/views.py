1329b1756d2990ae782b151d6e6c410afb110440
kiuter1 <ytigraman875@gmail.com>
2025-08-29 14:35:34 +0300

    refactor profile view for privacy profile

--- MiniInst/users/views.py
+++ MiniInst/users/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
-from .forms import CustomUserCreationForm
 from django.urls import reverse_lazy
 from django.contrib.auth import views as auth_views
+
+from .forms import CustomUserCreationForm
+from .models.follow import Follow
 from .models.custom_user import CustomUser
-from django.core.exceptions import PermissionDenied
 
 
 class CustomLoginView(auth_views.LoginView):
@@ -24,10 +25,41 @@ def register_view(request):
     return render(request, 'register.html', {'form': form})
 
 
+
 @login_required
 def profile_view(request, username):
     profile_user = get_object_or_404(CustomUser, username=username)
-    print('Looking for', profile_user) # перевірка чи підтягнувся юзернейм
-    if request.user.username != profile_user.username or profile_user.is_private == True:
-        raise PermissionDenied("Цей профіль закритий") # ось тут замінити на редірект до сторінки (таск в трело)
-    return render(request, 'profile.html', {'user': profile_user})
+    current_user = request.user
+
+    # Перевірка чи поточний користувач підписаний на профіль
+    is_following = Follow.objects.filter(
+        follower=current_user,
+        following=profile_user
+    ).exists()
+
+    can_view_details = (
+            current_user == profile_user or
+            not profile_user.is_private or
+            is_following
+    )
+
+    posts = []
+    posts_count = 0
+    if can_view_details:
+        posts = profile_user.posts.all()
+        posts_count = posts.count()
+
+    followers_count = profile_user.followers.count() if can_view_details else 0
+    following_count = profile_user.following.count() if can_view_details else 0
+
+    context = {
+        'profile_user': profile_user,
+        'is_following': is_following,
+        'can_view_details': can_view_details,
+        'posts': posts,
+        'posts_count': posts_count,
+        'followers_count': followers_count,
+        'following_count': following_count,
+    }
+
+    return render(request, 'profile.html', context)
