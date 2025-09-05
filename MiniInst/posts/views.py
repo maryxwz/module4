94c596dfde803b2f1e8f939629224db364d896b5
nigniger <viktorchobotar2022@gmail.com>
2025-09-05 22:30:05 +0300

    Update views.py

--- MiniInst/posts/views.py
+++ MiniInst/posts/views.py
@@ -13,3 +13,19 @@ def save_post(request, post_id):
 def saved_posts_view(request):
     saved_posts = SavedPost.objects.filter(user=request.user).select_related("post")
     return render(request, "saved_posts.html", {"saved_posts": saved_posts})
+@login_required
+@require_POST
+def toggle_like(request, post_id):
+    post = Post.objects.get(id=post_id)
+    like, created = Like.objects.get_or_create(user=request.user, post=post)
+
+    if not created:
+        like.delete()
+        liked = False
+    else:
+        liked = True
+
+    return JsonResponse({
+        'liked': liked,
+        'likes_count': post.likes.count()
+    })
