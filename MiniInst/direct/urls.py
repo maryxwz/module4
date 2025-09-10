3f0e7e93b80a5716660f3cbc494ce7083f8800ed
Yevf <yevfimiiloboda1276@gmail.com>
2025-09-10 20:06:35 +0100

    order pined chats
    # Conflicts:
    #	MiniInst/direct/views.py

--- MiniInst/direct/urls.py
+++ MiniInst/direct/urls.py
@@ -6,6 +6,7 @@ app_name = 'direct'
 urlpatterns = [
     path('inbox/', views.inbox, name='inbox'),
     path('toggle-pin/', views.toggle_pin, name='toggle_pin'),
+     path('reorder_pins/', views.reorder_pins, name='reorder_pins'),
     path('t/<str:kind>/<uuid:chat_id>/', views.thread_view, name='thread'),
     path('t/<str:kind>/<uuid:chat_id>/messages/', views.thread_messages_api, name='thread_messages_api'),
 ]
