2cc25f9bc2868eee2ac8a71019a14e38403584bc
Vova <vovinator2010@gmail.com>
2025-09-10 20:06:33 +0300

    cleaning up a bit

--- MiniInst/direct/consumers.py
+++ MiniInst/direct/consumers.py
@@ -216,20 +216,3 @@ class DirectConsumer(AsyncWebsocketConsumer):
             return False
         msg.delete()
         return True
-
-# ------------------ СПОВІЩЕННЯ ----------------------------------------------------------------------------------------
-class NotificationConsumer(AsyncWebsocketConsumer):
-    async def connect(self):
-        self.user = self.scope["user"]
-        if not self.user.is_authenticated:
-            await self.close()
-            return
-        self.group_name = f"user_{self.user.id}_notifications"
-        await self.channel_layer.group_add(self.group_name, self.channel_name)
-        await self.accept()
-
-    async def disconnect(self, close_code):
-        await self.channel_layer.group_discard(self.group_name, self.channel_name)
-
-    async def notify(self, event):
-        await self.send(text_data=json.dumps(event['data']))
