fa1b0609785d4588626d2f5360ab7186f1fac694
Yevf <yevfimiiloboda1276@gmail.com>
2025-09-17 18:02:41 +0100

    Create group on inbox page

--- MiniInst/direct/urls.py
+++ MiniInst/direct/urls.py
@@ -8,6 +8,8 @@ urlpatterns = [
     path('toggle-pin/', views.toggle_pin, name='toggle_pin'),
     path('create/', views.create_direct, name='create_direct'),
     path('reorder_pins/', views.reorder_pins, name='reorder_pins'),
+    path('get-friends/', views.get_friends, name='get_friends'),
+    path('create-group/', views.create_group, name='create_group'),
     path('t/<str:kind>/<uuid:chat_id>/', views.thread_view, name='thread'),
     path('t/<str:kind>/<uuid:chat_id>/messages/', views.thread_messages_api, name='thread_messages_api'),
 ]
