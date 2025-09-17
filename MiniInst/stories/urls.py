cba26e24559203d2c3e3fcc26398e627c3ac81de
Vova <vovinator2010@gmail.com>
2025-09-17 19:06:42 +0300

    Created a special page for browsing a single story

--- MiniInst/stories/urls.py
+++ MiniInst/stories/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('create_story/', views.add_story, name='create_story'),
     path('all_stories/', views.all_stories, name='all_stories'),
+    path('<int:id>/', views.view_story, name='current_story')
 ]
