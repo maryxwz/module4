938b37f6317f7891b24291d8f3b57856d761a388
nigniger <viktorchobotar2022@gmail.com>
2025-09-17 17:00:19 +0300

    Update models.py

--- MiniInst/posts/models.py
+++ MiniInst/posts/models.py
@@ -39,3 +39,23 @@ class Like(models.Model):
 
     def __str__(self):
         return f"{self.user.username} liked {self.post.id}"
+
+
+class Repost(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="reposts"
+    )
+    post = models.ForeignKey(
+        "posts.Post",
+        on_delete=models.CASCADE,
+        related_name="reposted_by"
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ("user", "post")
+
+    def __str__(self):
+        return f"{self.user.username} reposted {self.post.id}"
