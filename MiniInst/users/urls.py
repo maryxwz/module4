7b8e3c6a6b6aa63ad64ec2800b1cf65d2538f633
nigniger <viktorchobotar2022@gmail.com>
2025-09-17 16:54:40 +0300

    Update urls.py

--- MiniInst/users/urls.py
+++ MiniInst/users/urls.py
@@ -4,4 +4,9 @@ from .views import register_view, profile_view
 urlpatterns = [
     path('register/', register_view, name='register'),
     path('profile/', profile_view, name='profile'),
+    path("repost/<int:post_id>/", views.repost_post, name="repost_post"),
+    path("<str:username>/reposts/", views.user_reposts, name="user_reposts"),
 ]
+
+
+
